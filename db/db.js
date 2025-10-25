ca49a37d75c7bf407676043eceedc0add8dfda1e
Shazidul islam <shazidulislam910@gmail.com>
2025-10-25 12:06:59 +0600

    create user Scheme

---
+++ db/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+function connectTobd(){
+    mongoose.connect(process.env.MONGO_URI ).then(()=>{
+         console.log("Connect to DB")
+    }).catch(err=>console.log(err))
+}
+
+module.exports = connectTobd;
